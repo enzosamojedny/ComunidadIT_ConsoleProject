185d3c8eb526ad458f8d51b4e7d86b97f28fb801
enzosamojedny <enzosamojedny@gmail.com>
2024-09-11 13:52:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ComunidadIT_ConsoleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
